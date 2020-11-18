ae62b5c9e689d15b24172f45718ada1acc8592ca
davidcervantes99 <62263714+davidcervantes99@users.noreply.github.com>
2020-11-17 21:43:35 -0700

    Update sintactico.py

--- compilador/sintactico.py
+++ compilador/sintactico.py
@@ -1,5 +1,6 @@
-from compilador.lexico import Lexico, TOKENS, TOKENS_INV
-from compilador.errores import Error, ColeccionError
+from compilador.errores import ColeccionError, Error
+from compilador.lexico import TOKENS, TOKENS_INV, Lexico
+
 
 class Sintactico(object):
     def __init__(self, codigo=''):
@@ -160,15 +161,24 @@ class Sintactico(object):
     def FUNCION(self):
         if self.__verifica(TOKENS['FUNCTION']):
             self.__compara(self.complex.token)
+            if self.lexico.fin_definicion_variables_globales is None:
+                self.lexico.marcar_posicion(posicion='fin_definicion_variables_globales')
+
+            self.lexico.zona_de_codigo =Zonas.DEF_VARIABLES_LOCALES
+            self.lexico.marcar_posicion(posicion='inicio_defincion_variables_locales')
             if self.TIPO():
                 self.__compara(TOKENS['ID'])
                 self.__compara('(')
                 if self.PARAMETROS_FORMALES():
                     self.__compara(')')
-                    if self.DEFINIR_VARIABLES():
-                        if self.CUERPO_FUNCION():
-                            return True
+                if self.DEFINIR_VARIABLES():
+                    self.lexico.marcar_posicion(posicion='fin_definicion_variables_locales')
+                    self.lexico.zona_de_codigo= Zonas.CUERPO_FUNCION_LOCAL
+                if self.CUERPO_FUNCION():
+                    self.lexico.zona_de_codigo=Zonas.DEF_VARIABLES_GLOBALES
+                    return True
 
+        self.lexico.zona_de_codigo= Zonas.DEF_VARIABLES_GLOBALES
         return False
 
     def PARAMETROS_FORMALES(self):
@@ -604,6 +614,10 @@ class Sintactico(object):
     def PRINCIPAL(self):
         if self.__verifica(TOKENS['MAIN']):
             self.__compara(self.complex.token)
+            if self.lexico.fin_definicion_variables_globales is None:
+                self.lexico.marcar_posicion(posicion='fin_definicion_variables_globales')
+
+            self.lexico.zona_de_codigo= Zonas.CUERPO_PINCIPAL
             self.__compara('(')
             if self.PARAMETROS_FORMALES():
                 self.__compara(')')
@@ -614,4 +628,6 @@ class Sintactico(object):
                     self.__agregar_error(tipo='SINTACTICO', mensaje='Se esperaba un bloque de codigo')
 
         return False
+
+
 
