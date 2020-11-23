74448f73c9ab41be5d8be4c725c0e41f14d0825a
Claudia M. López B <33427303+mariannyy1002@users.noreply.github.com>
2020-11-22 21:33:31 -0700

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -28,6 +28,11 @@ const vm = new Vue({
         field: 'lexema',
         label: 'Lexema',
         centered: true
+      },
+      {
+        field: 'tipo',
+        label: 'Tipo',
+        centered: true
       }
     ],
     activeTab: 0
