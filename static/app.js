3721346eaede2957ce05219e1b9e1dade8ed6f66
DenisseVargas <62199618+DenisseVargas@users.noreply.github.com>
2020-11-23 23:21:43 -0700

    Update app.js

--- static/app.js
+++ static/app.js
@@ -5,9 +5,7 @@ const vm = new Vue({
     codigo: '',
     tokens: [],
     errores: [],
-    tablaSimbolos: [],
     resultadoExpresion: false,
-    resultadoPrograma: false,
     columnas: [
       {
         field: 'id',
@@ -20,24 +18,20 @@ const vm = new Vue({
         centered: true
       },
       {
-        field: 'codigo',
-        label: 'Codigo',
-        centered: true
-      },
-      {
         field: 'lexema',
         label: 'Lexema',
         centered: true
+      },
+      {
+        field:'tipo',
+        field: 'Tipo',
+        centered: true
       }
-    ],
-    activeTab: 0
+    ]
   },
   computed: {
     expresionValida() {
       return this.resultadoExpresion && this.errores.length === 0
-    },
-    programaValido() {
-      return this.resultadoPrograma && this.errores.length === 0
     }
   },
   methods: {
@@ -46,10 +40,6 @@ const vm = new Vue({
         this.tokens = response.data.componentes_lexicos.map(function(componente, indice) {
           componente['id'] = indice + 1
           return componente
-        })
-        this.tablaSimbolos = response.data.tabla_de_simbolos.map(function(simbolo, indice) {
-          simbolo['id'] = indice + 1
-          return simbolo
         })
         this.errores = response.data.errores.map(function(error, indice) {
           error['id'] = indice + 1
@@ -60,19 +50,6 @@ const vm = new Vue({
     compilaExpresion: _.debounce(function() {
       axios.post('/compila-expresion/', { codigo: this.codigo }, ).then((response) => {
         this.resultadoExpresion = response.data.expresion
-        this.errores = response.data.errores.map(function(error, indice) {
-          error['id'] = indice + 1
-          return error
-        })
-      })
-    }, 500),
-    compilaPrograma: _.debounce(function() {
-      axios.post('/compila-sintactico/', { codigo: this.codigo }, ).then((response) => {
-        this.resultadoPrograma = response.data.programa
-        this.tablaSimbolos = response.data.tabla_de_simbolos.map(function(simbolo, indice) {
-          simbolo['id'] = indice + 1
-          return simbolo
-        })
         this.errores = response.data.errores.map(function(error, indice) {
           error['id'] = indice + 1
           return error
