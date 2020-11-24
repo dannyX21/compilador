b825ca0b4cc735c7c9370cf1227dbe87424e91b5
PaolaMdz19 <62313195+PaolaMdz19@users.noreply.github.com>
2020-11-23 22:50:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def sintactico(methods=('get',)):
 def tabla_de_simbolos(*args, **kwargs):
     lexico = Lexico()
     simbolos = [{'token': s.token, 'lexema': s.lexema} for s in lexico.tabla_de_simbolos]
+    simbolos = [{'token': s.token, 'lexema': s.lexema, 'tipo': s.tipo} for s in lexico.tabla_de_simbolos]
     return ({'simbolos': simbolos}, 200)
 
 @app.route('/compila/', methods=('post',))
@@ -38,6 +39,8 @@ def compila(*args, **kwargs):
                     "token": componente_lexico.token,
                     "lexema": componente_lexico.lexema,
                     "codigo": componente_lexico.codigo
+                    "codigo": componente_lexico.codigo,
+                    "tipo": componente_lexico.tipo
                 }
             )
 
@@ -56,6 +59,9 @@ def compila(*args, **kwargs):
         'token': s.token,
         'lexema': s.lexema,
         'codigo': s.codigo
+        'codigo': s.codigo,
+        'tipo': s.tipo
+
     } for s in lexico.tabla_de_simbolos]
     return (resultado, 200)
 
@@ -85,6 +91,8 @@ def compila_sintactico(*args, **kwargs):
         'token': s.token,
         'lexema': s.lexema,
         'codigo': s.codigo
+        'codigo': s.codigo,
+        'tipo': s.tipo
     } for s in sintactico.lexico.tabla_de_simbolos]
     resultado['errores'] = [
         {
@@ -97,4 +105,5 @@ def compila_sintactico(*args, **kwargs):
     return (resultado, 200)
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(debug=True)
