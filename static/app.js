716002e050780db78af3997e5d2c626f6213fad4
Claudia M. López B <33427303+mariannyy1002@users.noreply.github.com>
2020-12-05 16:47:51 -0700

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -5,7 +5,10 @@ const vm = new Vue({
     codigo: '',
     tokens: [],
     errores: [],
+    tablaSimbolos: [],
+    tablaFunciones: [],
     resultadoExpresion: false,
+    resultadoPrograma: false,
     columnas: [
       {
         field: 'id',
@@ -18,15 +21,29 @@ const vm = new Vue({
         centered: true
       },
       {
+        field: 'codigo',
+        label: 'Codigo',
+        centered: true
+      },
+      {
         field: 'lexema',
         label: 'Lexema',
         centered: true
+      },
+      {
+        field: 'tipo',
+        label: 'Tipo',
+        centered: true
       }
-    ]
+    ],
+    activeTab: 0
   },
   computed: {
     expresionValida() {
       return this.resultadoExpresion && this.errores.length === 0
+    },
+    programaValido() {
+      return this.resultadoPrograma && this.errores.length === 0
     }
   },
   methods: {
@@ -35,6 +52,10 @@ const vm = new Vue({
         this.tokens = response.data.componentes_lexicos.map(function(componente, indice) {
           componente['id'] = indice + 1
           return componente
+        })
+        this.tablaSimbolos = response.data.tabla_de_simbolos.map(function(simbolo, indice) {
+          simbolo['id'] = indice + 1
+          return simbolo
         })
         this.errores = response.data.errores.map(function(error, indice) {
           error['id'] = indice + 1
@@ -45,6 +66,23 @@ const vm = new Vue({
     compilaExpresion: _.debounce(function() {
       axios.post('/compila-expresion/', { codigo: this.codigo }, ).then((response) => {
         this.resultadoExpresion = response.data.expresion
+        this.errores = response.data.errores.map(function(error, indice) {
+          error['id'] = indice + 1
+          return error
+        })
+      })
+    }, 500),
+    compilaPrograma: _.debounce(function() {
+      axios.post('/compila-sintactico/', { codigo: this.codigo }, ).then((response) => {
+        this.resultadoPrograma = response.data.programa
+        this.tablaSimbolos = response.data.tabla_de_simbolos.map(function(simbolo, indice) {
+          simbolo['id'] = indice
+          return simbolo
+        })
+        this.tablaFunciones = response.data.tabla_de_funciones.map(function(simbolo, indice) {
+          simbolo['id'] = indice
+          return simbolo
+        })
         this.errores = response.data.errores.map(function(error, indice) {
           error['id'] = indice + 1
           return error
