26b564f6edb7b5e060d3b52168b09a90e167d13e
PaolaMdz19 <62313195+PaolaMdz19@users.noreply.github.com>
2020-12-06 19:44:17 -0700

    Create semantico.py

---
+++ compilador/semantico.py
@@ -0,0 +1,26 @@
+class  Semantico:
+    temporales=(-1)
+    etiquetas=(-1)
+    def __init__ (self):
+        self.pila=[]
+
+
+    def push(self,elemento):
+        return self.pila.append(elemento)
+
+    def pop(self):
+        try:
+            return self.pila.pop()
+        except IndexError 
+        print ('No se puede eliminar, la pila esta vacia')
+
+    @staticmethod 
+    def generar_temporales ():
+        Semantico.temporales +=1
+        return ("_tmp"+repr(Semantico.temporales))
+
+
+    @staticmethod
+    def generar_etiqueta():
+        Semantico.etiquetas +=1
+        return("_Lbl"+repr(Semantico.etiquetas))
