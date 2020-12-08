29a15620aed53c036ec64f113c6d5293056eac36
Ivan Adrian Garcia Vargas <43709238+IvanGarVar@users.noreply.github.com>
2020-12-08 13:45:17 -0700

    Add files via upload

---
+++ compilador/semantico.py
@@ -0,0 +1,20 @@
+class Semantico:
+    indicetemporales=(-1)
+    indiceetiquetas=(-1)
+    def __init__(self):
+        self.pila=[]
+    def push(self, elemento):
+        return self.pila.append(elemento)
+    def pop(self):
+        try:
+            return self.pila.pop()
+        except IndexError:
+            print("Imposible Eliminar, Pila Vacia")
+    @staticmethod
+    def generar_temporal():
+        Semantico.indicetemporales+= 1
+        return("_tmp"+repr(Semantico.indicetemporales))
+    @staticmethod
+    def generar_etiqueta():
+        Semantico.indiceetiquetas+=1
+        return("Lbl"+repr(Semantico.indiceetiquetas))
